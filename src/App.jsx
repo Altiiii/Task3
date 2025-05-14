7ecc1846ee6cad7ff9f68a7c6aa977567cc3e368
Altin ismaili <ai30962@seeu.edu.mk>
2025-05-14 23:50:42 +0200

    First part about Header Component Finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header'
 
 function App() {
 
   return (
     <>
- TAS-20-test-github-codee
-        <h2 className='text-blue-800'>Testijm kejt me rend</h2>
-
-        <h2 className='text-blue-800'>Ishaffffllah bohet denisend</h2>
-        <h2 className='text-blue-800'>Ishaffffllah bohet denisend</h2>
-        <h2 className='text-blue-800'>Ishaffffllah bohet denisend</h2>
-        <h2 className='text-blue-800'>Ishaffffllah bohet denisend</h2>
-        <h2 className='text-blue-800'>Ishaffffllah bohet denisend zjarrr</h2>
-        <h2>Test Agaim</h2>
-
-
+        <Header />
     </>
   )
 }
