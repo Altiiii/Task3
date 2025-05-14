f2896217859cd987b88a764d004cd8db5760986f
Altin ismaili <ai30962@seeu.edu.mk>
2025-05-14 16:11:34 +0200

    TAS-20

--- src/App.jsx
+++ src/App.jsx
@@ -9,17 +9,11 @@ function App() {
     <>
  TAS-20-test-github-codee
         <h2 className='text-blue-800'>Testijm kejt me rend</h2>
-<<<<<<< HEAD
-        <h2 className='text-blue-800'>Ishaffffllah bohet denisend</h2>
-=======
-        <h2 className='text-blue-800'>Ishallah bohet denisend</h2>
 
-        <h2 className='text-green-800'>Testing jira</h2>
-        <h2 className='text-blue-800'>Here we test connection again</h2>
-        <h2 className='text-blue-800'>we are  we test connection again</h2>
-        <h2 className='text-blue-800'>Njo di tre</h2>
-main
->>>>>>> 302005748a89c9d31126729eba59802fd76ace50
+        <h2 className='text-blue-800'>Ishaffffllah bohet denisend</h2>
+        <h2>Test Agaim</h2>
+
+
     </>
   )
 }
